4125f09bbc54691651b4a660c9f385f1baeffb7f
zhong cheng liang <zchengliang@qq.com>
2024-02-21 09:57:22 +0800

    video achieve

--- src/main.cpp
+++ src/main.cpp
@@ -27,61 +27,53 @@ void resize_image(Mat& image){
     image = resizedImage;
 }
 int main() {
-    // 读取两张图片
-    Mat image1 = imread("/home/csoft/luyao/projects/merging_picture/piano_picture/1.jpg");
-    Mat image2 = imread("/home/csoft/luyao/projects/merging_picture/piano_picture/2.jpg");
-    Mat image3 = imread("/home/csoft/luyao/projects/merging_picture/piano_picture/3.jpg");
-    resize_image(image1);
-    resize_image(image2);
-    resize_image(image3);
-
-
-    if (image1.empty() || image2.empty() || image3.empty()) {
-        cerr << "Error: Could not load images." << endl;
+    VideoCapture cap1(41);
+    VideoCapture cap2(43);
+    VideoCapture cap3(45);
+    // 摄像头编号: 41 42    43 44   45 46
+    if (!cap1.isOpened() || !cap2.isOpened() || !cap3.isOpened()) {
+        cerr << "Error: Could not open camera." << endl;
         return -1;
     }
-    
-    // 合并两张图片  352   146 400   225
-    // resize(image1, image1, Size(image1.cols, image1.rows));
-    // resize(image, resizedImage, Size(newWidth, newHeight));
-    // resize(image, resizedImage, Size(newWidth, newHeight));
-    cout << "Image 1 size: " << image1.cols << " x " << image1.rows << endl;
-    cout << "Image 2 size: " << image2.cols << " x " << image2.rows << endl;
-    cout << "Image 3 size: " << image3.cols << " x " << image3.rows << endl;
+    Mat image1, image2, image3;   
+    while (true) {
+        // 从摄像头中读取图像
+        cap1.read(image1);
+        cap2.read(image2);
+        cap3.read(image3);
 
-    Mat croppedRegion1 = image1(Rect(0, 0, image1.cols, 352));
-    Mat croppedRegion2 = image2(Rect(0, 146, image1.cols, 400-146));
-    Mat croppedRegion3 = image3(Rect(0, 225, image3.cols, image3.rows-225));
-    // image1 = croppedRegion1;
-    // image2 = croppedRegion2;
-    // image3 = croppedRegion3;
-    rotate(croppedRegion1, image1, ROTATE_90_COUNTERCLOCKWISE);
-    rotate(croppedRegion2, image2, ROTATE_90_COUNTERCLOCKWISE);
-    rotate(croppedRegion3, image3, ROTATE_90_COUNTERCLOCKWISE);
+        // 对每个图像进行处理（缩小）
+        resize_image(image1);
+        resize_image(image2);
+        resize_image(image3);
 
-    Mat mergedImage = Mat::zeros(max(max(image1.rows, image2.rows), image3.rows), image1.cols + image2.cols + image3.cols, image1.type());
+        // 在这里添加其他对图像的处理，比如裁剪、旋转等
 
-    image1.copyTo(mergedImage(Rect(0, 0, image1.cols, image1.rows)));
-    image2.copyTo(mergedImage(Rect(image1.cols, 0, image2.cols, image2.rows)));
-    image3.copyTo(mergedImage(Rect(image1.cols + image2.cols, 0, image3.cols, image3.rows)));
+        // 合并三张图像
+        Mat mergedImage = Mat::zeros(max(max(image1.rows, image2.rows), image3.rows),
+                                     image1.cols + image2.cols + image3.cols, image1.type());
 
-    // 显示合并后的图片
-    // imshow("Image1", image1);
-    // imshow("Image2", image2);
-    // imshow("Image3", image3);
-    cout << "mergedImage size: " << mergedImage.cols << " x " << mergedImage.rows << endl;
-    imshow("mergedImage", mergedImage);
+        image1.copyTo(mergedImage(Rect(0, 0, image1.cols, image1.rows)));
+        image2.copyTo(mergedImage(Rect(image1.cols, 0, image2.cols, image2.rows)));
+        image3.copyTo(mergedImage(Rect(image1.cols + image2.cols, 0, image3.cols, image3.rows)));
 
+        // 显示合并后的图片
+        imshow("Merged Image", mergedImage);
 
-    // setMouseCallback("Image", onMouse, &mergedImage);
-    waitKey(0);
+        // 设置鼠标回调
+        // setMouseCallback("Merged Image", onMouse, &mergedImage);
 
-    // 处理按键重叠，这里简单地演示了图像融合，你可能需要更复杂的算法
-    // addWeighted(image1, 0.5, image2, 0.5, 0, image1);
-
-    // 显示处理后的图片
-    // imshow("Processed Image", image1);
-    // waitKey(0);
+        // 按Esc键退出循环
+        string userInput;
+        cin >> userInput;
+        if (userInput == "00") {
+            break;
+        }
+    }
+    // 释放摄像头
+    cap1.release();
+    cap2.release();
+    cap3.release();
 
     return 0;
 }
